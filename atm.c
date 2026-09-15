3cb94024567423b2a4c292668888d9e3d5fffe9f
EvvaJm <evva.morrissey@gmail.com>
2026-09-14 20:15:29 -0400

    Implement user menu for ATM options
    Added a menu for user options in ATM program.

--- atm.c
+++ atm.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
 
+int main (void){
+
+int choice; 
+  
+// prints menu of choices for the user to pick from 
+printf("WELCOME! Please Select an Option: \n");
+printf("1. Check Account Balance\n");
+printf("2. Deposit Money in your Account\n");
+printf("3. Withdraw Money from your Account\n");
+printf("4. Exit");
+scanf ("%d", &choice);
+
+if (choice
+  
+}
