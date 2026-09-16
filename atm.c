a88bdd28ae790dfef7121ed7bbfcdef0a0997445
EvvaJm <evva.morrissey@gmail.com>
2026-09-15 20:12:53 -0400

    Fix formatting and prompt messages in atm.c

--- atm.c
+++ atm.c
@@ -14,20 +14,19 @@ printf("WELCOME! Please Select an Option: \n");
 printf("1. Check Account Balance\n");
 printf("2. Deposit Money in your Account\n");
 printf("3. Withdraw Money from your Account\n");
-printf("4. Exit");
+printf("4. Exit\n");
 scanf ("%d", &choice);
 
 if (choice == 1){
   printf("Your balance is: %lf\n", balance);
 
 } else if (choice == 2){
-  printf("Please enter your canadian bills (5, 10, 20, 50 or 100) and enter X when your'e done: ")
+  printf("Please enter your canadian bills (5, 10, 20, 50 or 100) and enter X when your'e done: ");
   scanf("%d\n", &billEntered);
   while (billEntered != 'X' || billEntered != 'x'){
     depositTotal += billEntered;
    }
       
-  
+}
   
 }
-
