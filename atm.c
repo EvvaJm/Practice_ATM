b00ea826fde4635a1bdc7fb487e2c7449a2be751
EvvaJm <evva.morrissey@gmail.com>
2026-09-15 20:10:17 -0400

    Enhance ATM program with balance and input handling
    Added balance variable and improved menu options.

--- atm.c
+++ atm.c
@@ -3,6 +3,10 @@
 int main (void){
 
 int choice; 
+double balance = 1000.00;
+double deposit;
+int billEntered; 
+
   
 // prints menu of choices for the user to pick from 
 printf("WELCOME! Please Select an Option: \n");
@@ -12,6 +16,17 @@ printf("3. Withdraw Money from your Account\n");
 printf("4. Exit");
 scanf ("%d", &choice);
 
-if (choice
+if (choice == 1){
+  printf("Your balance is: %lf\n", balance);
+
+} else if (choice == 2){
+  printf("Please enter your canadian bills (5, 10, 20, 50 or 100) and enter X when your'e done: ")
+  scanf("%d\n", &billEntered);
+  while (billEntered != 'X' || billEntered != 'x'){
+    
+   }
+      
+  
   
 }
+
